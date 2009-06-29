04d5ad93fb4777ae214ca8785589ab19c04eb6ad
bnulwh <bnulwh@5d82d634-5cb5-11de-992f-fdd7e25b9163>
2009-06-29 02:55:14 +0000

    添加缺失文件

---
+++ TestLucene/ISUtils/Common/FieldInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISUtils.Common
+{
+    public sealed class FieldInfo
+    {
+        private bool primaryKey = false;
+        public bool PrimaryKey
+        {
+            get { return primaryKey; }
+            set { primaryKey = value; }
+        }
+        private string name = "";
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private Type type="".GetType();
+        public Type Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public FieldInfo()
+        { 
+        }
+        public FieldInfo(string name, Type type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+        public FieldInfo(string name, Type type,bool primaryKey)
+        {
+            this.name = name;
+            this.type = type;
+            this.primaryKey = primaryKey;
+        }
+    }
+}

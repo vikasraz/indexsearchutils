3379a3db81478638bdd64ecfe46d902596bf1cd5
bnulwh <bnulwh@5d82d634-5cb5-11de-992f-fdd7e25b9163>
2009-10-09 01:40:18 +0000

    Modify for change of config.xml

--- trunk/TestLucene/ISUtils/Indexer/IndexMaker.cs
+++ trunk/TestLucene/ISUtils/Indexer/IndexMaker.cs
@@ -18,12 +18,9 @@ namespace ISUtils.Indexer
     {
         #region Var
         private List<Source> sourceList;
-        private List<IndexSet> indexList;
         private FileIndexSet fileSet;
         private IndexerSet indexer;
         private DictionarySet dictSet;
-        private Dictionary<IndexSet,Source> ordinaryDict;
-        private Dictionary<IndexSet,Source> incremenDict;
         #endregion
         #region Contructor
         public IndexMaker(string filename)
@@ -75,7 +72,7 @@ namespace ISUtils.Indexer
         }
         #endregion
         #region Function
-        public bool CanIndex(TimeSpan span, IndexTypeEnum type)
+        public bool CanIndex(TimeSpan span, bool isIncreament)
         {
             if (type == IndexTypeEnum.Ordinary)
             {
@@ -94,64 +91,7 @@ namespace ISUtils.Indexer
             }
             return false;
         }
-#if DataBaseLib
-        public Message ExecuteBoostIndexer(DataBaseLibrary.SearchUpdateManage dblSum, TimeSpan span, IndexTypeEnum type)
-        {
-            Message msg = new Message();
-            if (CanIndex(span, type) == false)
-            {
-                msg.Result = "ExecuteIndexer does not run.";
-                msg.Success = false;
-                return msg;
-            }
-            try
-            {
-                if (type == IndexTypeEnum.Ordinary)
-                    BoostExecute(ordinaryDict, dictSet, indexer,dblSum, true, ref msg);
-                else
-                    BoostExecute(incremenDict, dictSet, indexer,dblSum,false, ref msg);
-                msg.Result = "ExecuteIndexer Success.";
-                msg.Success = true;
-                return msg;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Execute Indexer Error.Reason:" + e.Message);
-
-                msg.Result = "Exception:" + e.StackTrace.ToString();
-                msg.Success = false;
-                msg.ExceptionOccur = true;
-                return msg;
-            }
-        }
-        public static void BoostExecute(Dictionary<IndexSet, Source> dict, DictionarySet dictSet, IndexerSet indexer, DataBaseLibrary.SearchUpdateManage dblSum, bool create, ref Message msg)
-        {
-            try
-            {
-                DateTime allStart = DateTime.Now;
-                msg.AddInfo("All Start at :" + allStart.ToLocalTime());
-                Utils.IndexUtil.SetIndexSettings(dict, dictSet, indexer);
-                //由于中文分词结果随中文词库的变化而变化，为了使索引不需要根据中文词库的变化而变化，
-                //故采用默认的Analyzer来进行分词，即StandardAnalyzer
-                //Utils.IndexUtil.UseDefaultChineseAnalyzer(true);
-                Utils.IndexUtil.BoostIndex(dblSum,create);
-                msg.AddInfo("All End at :" + DateTime.Now.ToLocalTime());
-                TimeSpan allSpan = DateTime.Now - allStart;
-                msg.AddInfo(string.Format("All Spend {0} millionseconds.", allSpan.TotalMilliseconds));
-                msg.Success = true;
-                msg.Result = "Execute Success.";
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Execute Indexer Error.Reason:" + e.Message);
-                msg.AddInfo("Write Index Error.Reason:" + e.StackTrace.ToString());
-                msg.Success = false;
-                msg.ExceptionOccur = true;
-                throw e;
-            }
-        }
-#endif
-        public Message ExecuteBoostIndexer(TimeSpan span, IndexTypeEnum type)
+        public Message ExecuteBoostIndexer(TimeSpan span, bool isIncreament)
         {
             Message msg = new Message();
             if (CanIndex(span, type) == false)

2933fd2a6f3d259bdce87435cf6a3734dd75803c
bnulwh <bnulwh@5d82d634-5cb5-11de-992f-fdd7e25b9163>
2009-10-10 09:40:12 +0000

    Add new class for the two setting file

---
+++ trunk/TestLucene/ISUtils/Common/DbMonitorSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISUtils.Common
+{
+    /*
+  <DbMointor>
+  	<DbSuffix>_SSB</DbSuffix>
+  	<TvSuffix>_log</TvSuffix>
+  </DbMonitor>
+    */
+    [Serializable]
+    public class DbMonitorSet
+    {
+        #region 属性
+        private string dbSuffix = "_SSB";
+        public string DbSuffix
+        {
+            get { return dbSuffix; }
+            set { dbSuffix = value; }
+        }
+        private string monitorTable = "log";
+        public string MonitorTable
+        {
+            get { return monitorTable; }
+            set { monitorTable = value; }
+        }
+        #endregion
+    }
+}
